664d2e07beca926e85bac4e98ce18b580ab5fc7f
Henry <hdykhne@hotmail.ca>
2019-12-29 20:36:17 -0500

    trying new directory structure

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const PORT = 3000
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+app.get('/architech', (req, res) => {
+    res.send('Hello architech')
+})
+
+app.listen(PORT, () => {
+    console.log('server is up on port: ' + PORT)
+})
